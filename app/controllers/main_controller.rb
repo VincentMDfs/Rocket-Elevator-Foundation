5a1c625067c857c03c3d342463c7fe521aaba6d4
VincentMDfs <3471379@gmail.com>
2020-09-14 15:12:38 -0400

    production change

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -78,22 +78,22 @@ end
             )
         end
 # Sendgrid (finally) in a working state
-            from = SendGrid::Email.new(email: 'codeboxxvincentmd@gmail.com')
-            to = SendGrid::Email.new(email: params[:contact][:email])
-            subject = 'Contact form success'
-            content = Content.new(type: 'text/html', value: '<html><body><a href="https://relevator.xyz" title="Rocket Elevators"><img src="https://relevator.xyz'+ActionController::Base.helpers.image_url('R2small.png')+'" alt="Rocket logo" /></a><br /><br /><h3 style="color:red;">Greetings '+params[:contact][:name]+'</h3><br /><p style="color:red;">We thank you for contacting Rocket Elevators to discuss the opportunity to contribute to your project '+params[:contact][:project_name]+'. <br /><br /> A representative from our team will be in touch with you very soon. We look forward to demonstrate the value of our solutions and help you choose the appropriate product given your requirements.<br /></p>
-            <h5 style="color:blue;">We’ll Talk soon</h5><br />
-            <h3 style="color:red;">The Rocket Team</h3></body>
-            </html>')
+            # from = SendGrid::Email.new(email: 'codeboxxvincentmd@gmail.com')
+            # to = SendGrid::Email.new(email: params[:contact][:email])
+            # subject = 'Contact form success'
+            # content = Content.new(type: 'text/html', value: '<html><body><a href="https://relevator.xyz" title="Rocket Elevators"><img src="https://relevator.xyz'+ActionController::Base.helpers.image_url('R2small.png')+'" alt="Rocket logo" /></a><br /><br /><h3 style="color:red;">Greetings '+params[:contact][:name]+'</h3><br /><p style="color:red;">We thank you for contacting Rocket Elevators to discuss the opportunity to contribute to your project '+params[:contact][:project_name]+'. <br /><br /> A representative from our team will be in touch with you very soon. We look forward to demonstrate the value of our solutions and help you choose the appropriate product given your requirements.<br /></p>
+            # <h5 style="color:blue;">We’ll Talk soon</h5><br />
+            # <h3 style="color:red;">The Rocket Team</h3></body>
+            # </html>')
 
-            mail = SendGrid::Mail.new(from, subject, to, content)
-            sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-            response = sg.client.mail._('send').post(request_body: mail.to_json)
-            puts response.status_code
-            puts response.body
-            puts response.headers
+            # mail = SendGrid::Mail.new(from, subject, to, content)
+            # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+            # response = sg.client.mail._('send').post(request_body: mail.to_json)
+            # puts response.status_code
+            # puts response.body
+            # puts response.headers
             
-            redirect_to '/main/index', notice: "Message sent successfully"
+            # redirect_to '/main/index', notice: "Message sent successfully"
 
     end
     def download
--- lib/config/actions/rails_admin.rb
+++ lib/config/actions/rails_admin.rb
@@ -4,43 +4,43 @@ module RailsAdmin
       class Dashboard < RailsAdmin::Config::Actions::Base
         RailsAdmin::Config::Actions.register(self)
 	
-	require "ibm_watson/authenticators"
-	require "ibm_watson/text_to_speech_v1"
-	include IBMWatson
-        register_instance_option :root? do
-          true
-        end
+	# require "ibm_watson/authenticators"
+	# require "ibm_watson/text_to_speech_v1"
+	# include IBMWatson
+  #       register_instance_option :root? do
+  #         true
+  #       end
 
-        register_instance_option :breadcrumb_parent do
-          nil
-        end
+  #       register_instance_option :breadcrumb_parent do
+  #         nil
+  #       end
 
-        register_instance_option :controller do
-          proc do
+  #       register_instance_option :controller do
+  #         proc do
 
-		    authenticator = Authenticators::IamAuthenticator.new(
-		    apikey: ENV["WATSON_API_KEY"]
-		    )
+	# 	    authenticator = Authenticators::IamAuthenticator.new(
+	# 	    apikey: ENV["WATSON_API_KEY"]
+	# 	    )
 
-		    text_to_speech = IBMWatson::TextToSpeechV1.new(
-        authenticator: authenticator
-        )
+	# 	    text_to_speech = IBMWatson::TextToSpeechV1.new(
+  #       authenticator: authenticator
+  #       )
 
-		    text_to_speech.service_url = "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/12d9b61d-9be4-470f-bddd-679234cb7035"
-        employee = Employee.find_by(user_id: current_user.id)
-        File.open("app/assets/audios/watson.mp3", "wb") do |audio_file|
-          response = text_to_speech.synthesize(
-          text: "Good day",
-          accept: "audio/mp3",
-          voice: "en-GB_JamesV3Voice"
-          ).result
+	# 	    text_to_speech.service_url = "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/12d9b61d-9be4-470f-bddd-679234cb7035"
+  #       employee = Employee.find_by(user_id: current_user.id)
+  #       File.open("app/assets/audios/watson.mp3", "wb") do |audio_file|
+  #         response = text_to_speech.synthesize(
+  #         text: "Good day",
+  #         accept: "audio/mp3",
+  #         voice: "en-GB_JamesV3Voice"
+  #         ).result
 
-        audio_file.write(response)
+  #       audio_file.write(response)
         
-        end
-            render @action.template_name, status: 200
-          end
-        end
+  #       end
+        #     render @action.template_name, status: 200
+        #   end
+        # end
 
         register_instance_option :route_fragment do
           ''
